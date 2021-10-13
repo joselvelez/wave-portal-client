caba26fbe90113be7fb761d64ed24d637ed1c586
joselvelez <joselvelez@protonmail.com>
2021-10-13 11:27:53 -0500

    refactored wave logic to Wave component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { Wave } from './Wave';
 import { Transaction } from './Transaction';
-import abi from  './utils/WavePortal.json';
+// import abi from  './utils/WavePortal.json'; // Rinkeby
+import abi from  './utils/WavePortalLocalHost.json'; // Localhost
 
 function App() {
   const [currentTheme, setTheme] = useState('light');
@@ -11,8 +12,11 @@ function App() {
   const [lastWaveData, setLastWaveData] = useState();
   const [transaction, setTransaction] = useState();
 
-  const contractAddress = '0xa7B36508C42591aE327b3a160f5465406d9DA8E4';
-  const contractABI = abi.abi;
+  // const contractAddress = '0xa7B36508C42591aE327b3a160f5465406d9DA8E4'; // Rinkeby
+  const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'; // Localhost
+  // const contractABI = abi.abi; // Rinkeby
+  const contractABI = abi.abi; // Localhost
+
 
   // Confirm access to window.ethereum object
   const checkWalletConnection = async () => {
@@ -71,36 +75,9 @@ function App() {
         totalWaves: '',
       }
 
-      test = await wavePortalContract.lastWaveAt;
+      const test = await wavePortalContract.getLastWaveAt();
       // lastWaveObj.lastWaver = wavePortalContract.getLastWaveAt();
       console.log(test);
-
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  const wave = async () => {
-    try {
-      const { ethereum } = window;
-
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-
-        const waveTxn = await wavePortalContract.wave();
-        console.log("Mining transaction...", waveTxn);
-
-        await waveTxn.wait();
-        console.log("Mined...", waveTxn.hash);
-
-        let count = await wavePortalContract.getTotalWaves();
-        console.log("Retrieved total wave count...", count.toNumber());
-
-      } else {
-        console.log("Ethereum object does not exist!");
-      }
 
     } catch (e) {
       console.log(e);
@@ -132,7 +109,7 @@ function App() {
         </div>
 
         <div className="app">
-          <Wave account={currentAccount} connect={() => connectWallet()} wave={() => wave()} />
+          <Wave account={currentAccount} connect={() => connectWallet()} />
         </div>
 
         {lastWaveData ? 
--- src/Wave.js
+++ src/Wave.js
@@ -1,4 +1,41 @@
-export const Wave = ({ account, connect, wave }) => {
+import { ethers } from "ethers";
+// import abi from './utils/WavePortal.json'; // Rinkeby
+import abi from './utils/WavePortal.json'; // Localhost
+
+
+export const Wave = ({ account, connect }) => {
+    // const contractAddress = '0xa7B36508C42591aE327b3a160f5465406d9DA8E4'; // Rinkeby
+    const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'; // Localhost
+    // const contractABI = abi.abi; // Rinkeby
+    const contractABI = abi.abi; // Localhost
+
+    const wave = async () => {
+        try {
+          const { ethereum } = window;
+    
+          if (ethereum) {
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+    
+            const waveTxn = await wavePortalContract.wave();
+            console.log("Mining transaction...", waveTxn);
+    
+            await waveTxn.wait();
+            console.log("Mined...", waveTxn.hash);
+    
+            let count = await wavePortalContract.getTotalWaves();
+            console.log("Retrieved total wave count...", count.toNumber());
+    
+          } else {
+            console.log("Ethereum object does not exist!");
+          }
+    
+        } catch (e) {
+          console.log(e);
+        }
+      }
+
     return (
         <>
             {account ? 
