0d8571dcdcca42f78f8f52b7a3f15709bf783d80
joselvelez <joselvelez@protonmail.com>
2021-10-22 13:36:08 -0500

    added networks constant file

---
+++ src/constants/networks.js
@@ -0,0 +1,22 @@
+export const networks = {
+    0x1: {
+        name: "Ethereum Mainnet",
+        id: 1
+    },
+    0x3: {
+        name: "Ropsten Test Network",
+        id: 3
+    },
+    0x4: {
+        name: "Rinkeby Test Network",
+        id: 4
+    },
+    0x5: {
+        name: "Goerli Test Network",
+        id: 5
+    },
+    0x2a: {
+        name: "Kovan Test Network",
+        id: 42
+    },
+}
