a3d6a18074b07f225bc552b91406cf87cc3be27c
Jose L. Velez <joselvelez@protonmail.com>
2021-11-02 16:54:15 -0500

    refactor WrongChain component

--- src/components/core/WrongChain.js
+++ src/components/core/WrongChain.js
@@ -3,23 +3,24 @@ import { networks } from "../../constants/networks";
 
 export const WrongChain = () => {
     const [network, setNetwork] = useState({});
-    const ethereum = window.ethereum;
 
-    ethereum.on('chainChanged', () => {
+    window.ethereum.on('chainChanged', () => {
         window.location.reload();
     });
 
-    const fetchNetworks = async () => {
-        try{
-            const _chain = await ethereum.request({method: 'eth_chainId'});
-            const _network = networks.find(i => i.hex === _chain);
-            setNetwork(_network);
-        } catch (e) {
-            console.log("No network list found.");
-        }
-    };
-
     useEffect(() => {
+        const ethereum = window.ethereum;
+
+        const fetchNetworks = async () => {
+            try{
+                const _chain = await ethereum.request({method: 'eth_chainId'});
+                const _network = networks.find(i => i.hex === _chain);
+                setNetwork(_network);
+            } catch (e) {
+                console.log("No network list found.");
+            }
+        };
+
         fetchNetworks();
     }, []);
 
