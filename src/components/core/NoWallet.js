3f3bcb1f50e1b549b0214a26b5549cd8f065c5a1
Jose L. Velez <joselvelez@protonmail.com>
2021-11-12 16:22:28 -0600

    update nowallet component

--- src/components/core/NoWallet.js
+++ src/components/core/NoWallet.js
@@ -1,7 +1,10 @@
 export const NoWallet = () => {
     return (
         <div>
-            <p>You need a wallet installed to access this site!</p>
+            <h2>You need a wallet installed to access this site!</h2>
+            <br />
+            <p>This web3 application is built to operate with the <a href="https://metamask.io/">MetaMask</a> wallet. Visit the MetaMask site and install the wallet to access this app.</p>
+            <br />
         </div>
     )
 }
